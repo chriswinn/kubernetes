06cb899a3a0d02872ce8425e6ead7b09731794a2
Chris Winn <chriswinn@gmail.com>
2019-09-20 16:06:22 -0400

    local_SETUP.sh file is not complete, just adding it in it's current meh state.

---
+++ local_SETUP.sh
@@ -0,0 +1,26 @@
+#################################################
+# How to set up your first Kubernetes environment on macOS
+#################################################
+#
+# 1. A way to run containers
+#################################################
+brew update && brew install kops
+#brew install kubectl 
+#brew install kops
+
+#
+# 2. Kubernetes
+#################################################
+#
+# 3. The Kubernetes command-line tool, kubectl.
+#################################################
+#
+# 4. The oc command-line tool for Red Hat OpenShift
+#################################################
+#
+# 5. An image to run as a test.
+#################################################
+
+
+
+
